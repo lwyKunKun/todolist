a21d0205842491eeb07c0db04c5b7e59402a1232
lwyKunKun <458426099@qq.com>
2021-06-07 23:33:41 +0800

    测试代码

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,12 +1,13 @@
 <template>
-  <footer class="main-footer">Written By lwy</footer>
+  <footer class="main-footer">Written By lwy1</footer>
 </template>
 
 
 <style lang="stylus" scoped>
-  .main-footer
-    margin-top: 20px
-    text-align: center
-    color: #fff
-    text-shadow: 5px 5px 5px #000
+.main-footer {
+  margin-top: 20px;
+  text-align: center;
+  color: #fff;
+  text-shadow: 5px 5px 5px #000;
+}
 </style>
